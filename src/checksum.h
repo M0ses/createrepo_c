6ec3adaac5a1bfbbe8ddf680b7583a8b71cc076f
Frank Schreiner <schreiner@suse.de>
2023-02-24 13:18:58 +0100

    add support for external checksums file
    With this patch createrepo_c will process pre-built checksum lists taken
    from the evironment variable `CREATEREPO_CHECKSUMS`.
    If a package was found in the list the checksum is taken from the list
    instead of creating the checksum of the package.
    This can speed up createrepo_c significantly and reduce IO load in build
    environments which provide those pre-built lists.

--- src/checksum.h
+++ src/checksum.h
@@ -42,8 +42,8 @@ typedef struct _cr_ChecksumCtx cr_ChecksumCtx;
  */
 typedef enum {
     CR_CHECKSUM_UNKNOWN  = 0,   /*!< Unknown checksum */
-#ifdef WITH_LEGACY_HASHES
     CR_CHECKSUM_MD5      = 1,   /*!< MD5 checksum */
+#ifdef WITH_LEGACY_HASHES
     CR_CHECKSUM_SHA      = 2,   /*!< SHA checksum */
     CR_CHECKSUM_SHA1     = 3,   /*!< SHA1 checksum */
 #endif
@@ -66,6 +66,12 @@ const char *cr_checksum_name_str(cr_ChecksumType type);
  * @return              checksum type
  */
 cr_ChecksumType cr_checksum_type(const char *name);
+
+/** Return size of raw checksum
+ * @param type          checksum type
+ * @return              size of raw checksum or 0 on error
+ */
+size_t cr_checksum_raw_size(cr_ChecksumType type);
 
 /** Compute file checksum.
  * @param filename      filename
@@ -104,6 +110,14 @@ int cr_checksum_update(cr_ChecksumCtx *ctx,
  * @return          Checksum string or NULL on error.
  */
 char *cr_checksum_final(cr_ChecksumCtx *ctx, GError **err);
+
+/** Finalize checksum calculation, fill raw_checksum buffer and return checksum length.
+ * Free all checksum context resources.
+ * @param ctx       Checksum context.
+ * @param err       GError **
+ * @return          Raw checksum length or 0 on error.
+ */
+size_t cr_checksum_final_raw(cr_ChecksumCtx *ctx, unsigned char *raw_checksum, GError **err);
 
 /** @} */
 
